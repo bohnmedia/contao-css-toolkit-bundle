779048686bd5122e3a4289e8a952b920b56902c8
Dennis Bohn <dennis.bohn@bohn.media>
2019-07-31 21:07:38 +0000

    third commit

--- src/Resources/contao/dca/tl_cssutils.php
+++ src/Resources/contao/dca/tl_cssutils.php
@@ -18,8 +18,8 @@ $GLOBALS['TL_DCA']['tl_cssutils'] = array
 	(
 		'sorting' => array
 		(
-			'mode'			=> 4,
-			'fields'		=> array('name'),
+			'mode'		=> 4,
+			'fields'	=> array('name'),
 			'panelLayout'	=> 'filter;sort,search,limit',
 			'headerFields'	=> array('name', 'author', 'tstamp')
 		),
@@ -36,31 +36,31 @@ $GLOBALS['TL_DCA']['tl_cssutils'] = array
 		(
 			'edit' => array
 			(
-				'href'                => 'act=edit',
-				'icon'                => 'edit.svg'
+				'href'		=> 'act=edit',
+				'icon'		=> 'edit.svg'
 			),
 			'copy' => array
 			(
-				'href'                => 'act=paste&amp;mode=copy',
-				'icon'                => 'copy.svg',
-				'attributes'          => 'onclick="Backend.getScrollOffset()"'
+				'href'		=> 'act=paste&amp;mode=copy',
+				'icon'		=> 'copy.svg',
+				'attributes'	=> 'onclick="Backend.getScrollOffset()"'
 			),
 			'cut' => array
 			(
-				'href'                => 'act=paste&amp;mode=cut',
-				'icon'                => 'cut.svg',
-				'attributes'          => 'onclick="Backend.getScrollOffset()"'
+				'href'		=> 'act=paste&amp;mode=cut',
+				'icon'		=> 'cut.svg',
+				'attributes'	=> 'onclick="Backend.getScrollOffset()"'
 			),
 			'delete' => array
 			(
-				'href'                => 'act=delete',
-				'icon'                => 'delete.svg',
-				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+				'href'		=> 'act=delete',
+				'icon'		=> 'delete.svg',
+				'attributes'	=> 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
 			),
 			'show' => array
 			(
-				'href'                => 'act=show',
-				'icon'                => 'show.svg'
+				'href'		=> 'act=show',
+				'icon'		=> 'show.svg'
 			)
 		)
 	),
